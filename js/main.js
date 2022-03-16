dc0ed0964da0b971e04dd9345e35b718911f20b1
Laskoviy <mzhidelev@yandex.ru>
2022-03-16 15:32:22 +0300

    first try

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { HOTELS_COUNT } from './const.js';
 import { createHotel } from './data.js';
+import { createPopup } from './utils/popup.js';
 
 // Создание массива с отелями
 const hotels = new Array(HOTELS_COUNT).fill().map(createHotel);
@@ -7,3 +8,8 @@ const hotels = new Array(HOTELS_COUNT).fill().map(createHotel);
 //Заглушка для линтера
 const addHotels = () => { };
 addHotels(hotels);
+
+//Заглушка для линтера
+hotels.forEach((hotel) => {
+  createPopup(hotel);
+});
