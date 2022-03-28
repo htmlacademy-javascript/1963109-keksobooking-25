859df6c0855344057dda3b51b03fe57233998484
Laskoviy <mzhidelev@yandex.ru>
2022-03-28 16:25:59 +0400

    module8-task2

--- js/utils/work-with-form.js
+++ js/utils/work-with-form.js
@@ -1,12 +1,16 @@
 import '../../pristine/pristine.min.js';
 /*eslint-disable*/
 export const initFormValidate = () => {
+  const appartmentsType = document.querySelector('#type');
+  const appartmentsPrice = document.querySelector('#price');
   const form = document.querySelector('.ad-form');
   const capacity = document.querySelector('#capacity');
   const roomNumber = document.querySelector('#room_number');
   const guests = [...capacity.children];
+  const timein = document.querySelector('#timein');
+  const timeout = document.querySelector('#timeout');
 
-  // Синхронизация полей «Количество комнат» и «Количество мест»
+  // Синхронизация полей «Количество комнат» и «Количество мест» задание 8.1
   const enableGuests = (selectedRooms) => {
     guests.slice().reverse().forEach((guest, index) => {
       if (index <= selectedRooms && index !== 0 && selectedRooms !== '100') {
@@ -26,6 +30,34 @@ export const initFormValidate = () => {
       }
     });
   };
+
+  // Логика выбора типа жилья (Задание 8.2)
+  appartmentsType.addEventListener('change', (event) => {
+    switch (event.target.value) {
+      case 'flat':
+        appartmentsPrice.setAttribute('min', 1000);
+        appartmentsPrice.setAttribute('placeholder', 'от 1000 у.е.');
+        break;
+      case 'bungalow':
+        appartmentsPrice.setAttribute('min', 0);
+        appartmentsPrice.setAttribute('placeholder', 'от 0 у.е.');
+        break;
+      case 'house':
+        appartmentsPrice.setAttribute('min', 5000);
+        appartmentsPrice.setAttribute('placeholder', 'от 5000 у.е.');
+        break;
+      case 'palace':
+        appartmentsPrice.setAttribute('min', 10000);
+        appartmentsPrice.setAttribute('placeholder', 'от 10000 у.е.');
+        break;
+      case 'hotel':
+        appartmentsPrice.setAttribute('min', 3000);
+        appartmentsPrice.setAttribute('placeholder', 'от 3000 у.е.');
+        break;
+      default:
+        break;
+    }
+  });
 
   roomNumber.addEventListener('change', (event) => {
     enableGuests(event.target.value);
@@ -50,3 +82,33 @@ export const initFormValidate = () => {
   });
 }
 
+// Синхронизация времени заезда и времени выезда (Задание 8.2)
+const timeinArr = [...timein.children];
+const timeoutArr = [...timeout.children];
+
+const conditionTime = (arr, currentTime) => {
+  arr.forEach((time) => {
+    if (time.value === currentTime.value) {
+      time.setAttribute('selected', 'selected');
+    } else {
+      time.removeAttribute('selected');
+    }
+  });
+};
+
+const syncTime = (target, str) => {
+  if (str === 'in') {
+    conditionTime(timeoutArr, target);
+  }
+  if (str === 'out') {
+    conditionTime(timeinArr, target);
+  }
+};
+
+timein.addEventListener('change', (event) => {
+  syncTime(event.target, 'in');
+});
+
+timeout.addEventListener('change', (event) => {
+  syncTime(event.target, 'out');
+});
